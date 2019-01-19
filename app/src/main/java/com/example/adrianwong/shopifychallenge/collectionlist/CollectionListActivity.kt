62dc05aa0f18bcf69823d1617567ee5a8f4a8180
adrianwg23 <adrianwg23@hotmail.com>
2019-01-19 15:56:17 -0500

    Initial commit

---
+++ app/src/main/java/com/example/adrianwong/shopifychallenge/collectionlist/CollectionListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.adrianwong.shopifychallenge.collectionlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.adrianwong.shopifychallenge.R
+
+class CollectionListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_collection_list)
+    }
+}
